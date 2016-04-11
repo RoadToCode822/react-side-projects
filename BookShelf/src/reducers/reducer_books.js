c1f9cc9b53d742c6ad1c7eac51196491b2216e39
Mahtab <roadtocode822@gmail.com>
2016-04-11 01:20:20 -0400

    Added files via upload

---
+++ BookShelf/src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+  return [
+    { title: 'Javascript: The Good Parts', pages: 101 },
+    { title: 'Crucial Conversations', pages: 205 },
+    { title: 'Javascript: Understanding the Weird Parts', pages: 105 },
+    { title: 'American Icon' , pages: 110},
+    { title: 'Your Network is your Net Worth', pages: 310 }
+  ]
+}
